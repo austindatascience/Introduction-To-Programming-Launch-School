fb798983832e18eb5be8376cd768d8f680f0c770
Austin Pacheco Timmerman <33733306+austinpianodev@users.noreply.github.com>
2019-03-14 13:04:04 -0700

    Create exercise1.rb

---
+++ Hashes/exercise1.rb
@@ -0,0 +1,9 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+          
+immediate_family = family.select { |k, v| k == :sisters || k == :brothers }
+new_array = immediate_family.values.flatten
+puts new_array
